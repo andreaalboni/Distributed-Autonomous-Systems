47e1268433ca82621184acc04755af6d257203f8
Emanuele Monsellato <emanuele.monsellato@studio.unibo.it>
2025-05-06 15:54:30 +0200

    task2 start

---
+++ exam_ws/src/task2/task2/aggregative_tracking.py
@@ -0,0 +1,7 @@
+# Aggregative Optimization problem -> implement the algorithm Aggregative Tracking algorithm
+
+import numpy as np
+import networkx as nx
+import matplotlib.pyplot as plt
+
+from utils import *
