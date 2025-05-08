b5b3749874cbaab6a713a229968cab462cb92485
andreaalboni <291587@studenti.unimore.it>
2025-05-08 11:01:09 +0200

    graficando cose

--- exam_ws/src/task1/task1/config.py
+++ exam_ws/src/task1/task1/config.py
@@ -4,8 +4,8 @@ PARAMETERS = {
     'num_targets': 2,
     'ratio_at': 5,
     'world_size': [5, 5],
-    'radius_fov': 1.0, # np.inf,
-    'noise_level': 0.1,
+    'radius_fov': np.inf, # np.inf,
+    'noise_level': 0.0,
     'bias': 0.0,
     'p_er': 0.5,
 }
--- exam_ws/src/task1/task1/gradient_tracking.py
+++ exam_ws/src/task1/task1/gradient_tracking.py
@@ -9,13 +9,15 @@ def gradient_tracking_method(max_iters=1500, alpha=0.001, save=False):
     targets, agents = generate_agents_and_targets()
     # visualize_world(agents, targets, world_size=params['world_size'])
     real_distances, noisy_distances = get_distances(agents, targets)
-    graph_type = 'erdos_renyi'
+    graph_type = 'cycle'
     _, adj, A = generate_graph(len(agents), type=graph_type)
     # visualize_graph(G)
     
     z_opt = get_targets_real_positions(targets)
     
     cost = np.zeros((max_iters))
+    norm_grad_cost = np.zeros((max_iters))
+    prova = np.zeros((max_iters))
     z = np.zeros((max_iters, len(agents), len(targets), targets.shape[1]))
     s = np.zeros((max_iters, len(agents), len(targets), targets.shape[1]))
     
@@ -58,6 +60,7 @@ def gradient_tracking_method(max_iters=1500, alpha=0.001, save=False):
         # --------------------------------------------------------
         '''
         
+        total_grad = np.zeros_like(s[0])
         for i in range(len(agents)):
             z[k+1, i] = A[i, i] * z[k, i]
             N_i = np.nonzero(adj[i])[0]
@@ -74,13 +77,26 @@ def gradient_tracking_method(max_iters=1500, alpha=0.001, save=False):
             _, grad_l_i_new = local_cost_function(z[k+1, i], agents[i], real_distances[i])
             l_i, grad_l_i_old = local_cost_function(z[k, i], agents[i], real_distances[i])
             s[k+1, i] += grad_l_i_new - grad_l_i_old
+            
+            total_grad += s[k+1,i]
             cost[k] += l_i
+
+        norm_grad_cost[k] = np.linalg.norm(total_grad / len(agents))
+        prova[k] = np.linalg.norm(s[k,0])
     
     fig, axes = plt.subplots(figsize=(8, 6), nrows=1, ncols=2)
     ax = axes[0]
-    ax.semilogy(np.arange(max_iters-1), cost[:-1])
+    ax.semilogy(np.arange(max_iters-1), cost[:-1], color='violet')
     ax.set_title('Cost vs Iteration')
     ax.set_xlabel('Iteration')
+    
+    ax = axes[1]
+    ax.semilogy(np.arange(max_iters-1), norm_grad_cost[:-1], color='cyan')
+    ax.semilogy(np.arange(max_iters-1), prova[:-1], color='purple')
+    ax.set_title('Gradient of the cost vs Iteration')
+    ax.set_xlabel('Iteration')
+    
+    """
     ax = axes[1]
     for i in range(len(targets)):
         for j in range(len(agents)):
@@ -88,7 +104,8 @@ def gradient_tracking_method(max_iters=1500, alpha=0.001, save=False):
             ax.semilogy(np.arange(max_iters-1), errors)
     # ax.legend()
     ax.set_title('Estimation error vs Iteration')
-    ax.set_xlabel('Iteration')
+    ax.set_xlabel('Iteration') 
+    """
     plt.show()
     
     # print(f"z optimal: {z_opt}")
@@ -98,73 +115,6 @@ def gradient_tracking_method(max_iters=1500, alpha=0.001, save=False):
     animate_world_evolution(agents, targets, type=graph_type, z_hystory=z)
     return z, cost
 
-def doll_gradient_tracking_method(max_iters=400, alpha=0.001):
-    # -------------- Not working yet ------------------
-    targets, agents = generate_agents_and_targets(
-        num_targets=params['num_targets'],
-        ratio_at=params['ratio_at'],
-        world_size=params['world_size'],
-        radius_fov=params['radius_fov']
-    )
-    #visualize_world(agents, targets, world_size=params['world_size'])
-    real_distances, noisy_distances = get_distances(agents, targets)
-    G, adj, A = generate_graph(len(agents), type='erdos_renyi', p_er=0.5)
-    #visualize_graph(G)
-    
-    Q = np.zeros((len(agents), len(targets), len(targets)))
-    for i in range(len(agents)):
-        Q[i] = np.eye(len(targets)) * 3
-    
-    z_opt = get_targets_real_positions(targets)
-    
-    cost = np.zeros((max_iters))
-    z = np.zeros((max_iters, len(agents), len(targets), targets.shape[1]))
-    s = np.zeros((max_iters, len(agents), len(targets), targets.shape[1]))
-    
-    z[0] = np.random.uniform(params['world_size'][0]/2, params['world_size'][1]/2, z[0].shape)
-        
-    for i in range(len(agents)):
-        _, s[0, i] = doll_cost_function(z[0, i], Q[i], noisy_distances[i])
-
-    for k in range(max_iters - 1):
-        for i in range(len(agents)):
-            z[k+1, i] = A[i, i] * z[k, i]
-            N_i = np.nonzero(adj[i])[0]
-            for j in N_i:
-                z[k+1, i] += A[i, j] * z[k, j]
-            z[k+1, i] -= alpha * s[k, i]
-        
-        for i in range(len(agents)):
-            s[k+1, i] = A[i, i] * s[k, i]
-            N_i = np.nonzero(adj[i])[0]
-            for j in N_i:
-                s[k+1, i] += A[i, j] * s[k, j]
-            
-            _, grad_l_i_new = doll_cost_function(z[k+1, i], Q[i], noisy_distances[i])
-            _, grad_l_i_old = doll_cost_function(z[k, i], Q[i], noisy_distances[i])
-            s[k+1, i] += grad_l_i_new - grad_l_i_old
-            
-            l_i, _ = doll_cost_function(z[k, i], Q[i], noisy_distances[i])
-            cost[k] += l_i
-    
-    fig, axes = plt.subplots(figsize=(8, 6), nrows=1, ncols=2)
-    ax = axes[0]
-    ax.semilogy(np.arange(max_iters-1), cost[:-1])
-    ax.set_title('Cost vs Iteration')
-    ax.set_xlabel('Iteration')
-    ax = axes[1]
-    for i in range(len(targets)):
-        for j in range(len(agents)):
-            errors = [np.linalg.norm(z[t, j, i] - z_opt[0, i]) for t in range(max_iters-1)]
-            ax.semilogy(np.arange(max_iters-1), errors, label=f'Agent {j+1}, Target {i+1}')
-    #ax.legend()
-    ax.set_title('Estimation error vs Iteration')
-    ax.set_xlabel('Iteration')
-    ax.grid(True)
-    plt.show()
-    
-    animate_world_evolution(agents, targets, world_size=params['world_size'], z_hystory=z)
-    return z, cost
 
 def main():
     gradient_tracking_method()
