cd56fd90c04f5b9c3cabaf5c0f6beefc0e3ec12f
FedericoCalzoni <federicocalzoni01@gmail.com>
2025-05-13 20:32:08 +0200

    params

--- exam_project/task1/main.py
+++ exam_project/task1/main.py
@@ -10,12 +10,10 @@ from gradient_tracking import gradient_tracking_algorithm
 
 def main():
     
-    save = False
     # Initialization
     targets, agents = generate_agents_and_targets()
     real_distances, noisy_distances = get_distances(agents, targets)
-    graph_type = 'cycle'
-    G, adj, A = generate_graph(len(agents), type=graph_type, p_er=PARAMETERS['p_er'])
+    G, adj, A = generate_graph(len(agents), type=PARAMETERS['graph_type'], p_er=PARAMETERS['p_er'])
     
     # Run traking algorithm
     z, cost, norm_grad_cost, prova = gradient_tracking_algorithm(agents,
@@ -27,13 +25,13 @@ def main():
     
     # Visualization
     visualize_world(agents, targets, world_size=PARAMETERS['world_size'])
-    plot_gradient_traking_results(z, cost, norm_grad_cost, prova, agents, targets, graph_type)
+    plot_gradient_traking_results(z, cost, norm_grad_cost, prova, agents, targets, PARAMETERS['graph_type'])
     print(f"Final error norm: {np.linalg.norm(PARAMETERS['world_size'] * z[-1,0] - PARAMETERS['world_size'] * targets)}")
-    animate_world_evolution(agents, targets, type=graph_type, z_history=z, world_size=PARAMETERS['world_size'])
+    animate_world_evolution(agents, targets, type=PARAMETERS['graph_type'], z_history=z, world_size=PARAMETERS['world_size'])
     
-    # Save data if requested
-    if save:
-        save_evolution_data(agents, targets, z, type=graph_type, world_size=PARAMETERS['world_size'])
+    
+    if False:
+        save_evolution_data(agents, targets, z, type=PARAMETERS['graph_type'], world_size=PARAMETERS['world_size'])
 
 
 if __name__ == "__main__":
